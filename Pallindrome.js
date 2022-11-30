9ba04723d21a903ffb27b4d1f7c0dd0c1ffc751d
omkarSw2 <119350956+omkarSw2@users.noreply.github.com>
2022-11-30 19:59:47 +0530

    Create Pallindrome.js

---
+++ Pallindrome.js
@@ -0,0 +1,11 @@
+let arr="afifa";
+// bag="";
+// for(i=arr.length-1;i>=0;i--){
+//   bag=bag+arr[i];
+
+// }
+// if(arr==bag){
+//   console.log("Pallindrome");
+// }else{
+//   console.log("Not Pallindrome");
+//     }
